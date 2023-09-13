65938787f2c9cf933d3cd0bab40024c20b633de3
ivm <ivm@fnal.gov>
2023-09-13 10:47:20 -0500

    .

--- rucio_consistency/config.py
+++ rucio_consistency/config.py
@@ -250,7 +250,6 @@ class ConfigRucioBackend(ConfigBackend):
             #print(f"Rucio backend: get_root({root}, {rse}): cfg:", cfg)
             return cfg
 
-
 class CEConfiguration(object):
     
     def __init__(self, rse, source, **args):
@@ -276,6 +275,65 @@ class CEConfiguration(object):
         try:    return self[name]
         except KeyError: return default
 
+class MergedCEConfiguration(object):
+
+    CONFIG_PREFIX = "CE_config."
+    """
+            Enable/disable CE for the site
+            Thresholds for CE actions (fraction of dark/missing files to abort the action)
+            List of roots to scan
+            List of ignored directories
+            Scanning concurrency
+            Scanning timeout
+            Server address
+    """
+
+    def __init__(self, rse, config_file, account="root"):
+        from rucio.client.rseclient import RSEClient
+        self.RSEClient = RSEClient(account=account)
+        self.RSE = rse
+        self.ConfigFromFile = yaml.load(open(config_file, "r"), Loader=yaml.SafeLoader)
+        self.ConfigFromRSE = self.config_from_rse()
+
+    def config_from_rse(self):
+        rse_config = self.RSEClient.list_rse_attributes(rse.upper())
+        cfg = {}
+        if self.CONFIG_PREFIX+"disabled" in rse_config:     
+            cfg["enabled"] = rse_config[self.CONFIG_PREFIX+"disabled"] == "yes"
+        if self.CONFIG_PREFIX+"ignore_list" in rse_config: 
+            cfg["ignore_list"] = rse_config[self.CONFIG_PREFIX+"ignore_list"].split(",")
+
+        scanner_cfg = {}
+        if self.CONFIG_PREFIX+"server" in rse_config:          
+            scanner_cfg["server"] = rse_config[self.CONFIG_PREFIX+"server"]
+        if self.CONFIG_PREFIX+"server_root" in rse_config:     
+            scanner_cfg["server_root"] = rse_config[self.CONFIG_PREFIX+"server_root"]
+        if self.CONFIG_PREFIX+"roots" in rse_config:           
+            scanner_cfg["roots"] = [{"path":path} for path in rse_config[self.CONFIG_PREFIX+"roots"].split(",")]
+        if self.CONFIG_PREFIX+"nworkers" in rse_config:        
+            scanner_cfg["nworkers"] = int(rse_config[self.CONFIG_PREFIX+"nworkers"])
+        if self.CONFIG_PREFIX+"timeout" in rse_config:         
+            scanner_cfg["timeout"] = int(rse_config[self.CONFIG_PREFIX+"timeout"])
+        cfg["scanner"] = scanner_cfg
+
+        if self.CONFIG_PREFIX+"max_dark_fraction" in rse_config:       
+            cfg["dark_action"] = {"max_fraction":float(rse_config[self.CONFIG_PREFIX+"max_dark_fraction"])}
+        if self.CONFIG_PREFIX+"max_missing_fraction" in rse_config:    
+            cfg["missing_action"] = {"max_fraction":float(rse_config[self.CONFIG_PREFIX+"max_missing_fraction"])}
+        return cfg
+
+    def merge(self, defaults, overrides):
+        out = defaults.copy()
+        for key, value in overrides.items():
+            if isinstance(value, dict):
+                out[key] = merge(defaults.get(key, {}), value)
+            else:
+                out[key] = value
+        return out
+
+    def config(self):
+        rse_config = self.merge(self.ConfigFromFile["rses"].get("*", {}), self.ConfigFromFile["rses"].get(rse, {}))
+        return merge(rse_config, self.ConfigFromRSE)
 
 class ScannerConfiguration(CEConfiguration):
     
@@ -300,12 +358,12 @@ class ScannerConfiguration(CEConfiguration):
 
 if __name__ == "__main__":
     import sys, getopt
-    opts, args = getopt.getopt(sys.argv[1:], "c:r")
+    opts, args = getopt.getopt(sys.argv[1:])
     opts = dict(opts)
-    rse, param = args[:2]
+    rse, config_file = args[:2]
+    cfg = MergedCEConfiguration(rse, config_file)
+    yaml.dump(cfg.config())
     
-    cfg = CEConfiguration(rse, opts["-c"])
-    print(getattr(cfg, param))
         
         
     
