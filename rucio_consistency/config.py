9d0ba14e9034a077b0685f3c295e930a5e8edcd0
ivm <ivm@fnal.gov>
2023-09-18 06:44:38 -0500

    .

--- rucio_consistency/config.py
+++ rucio_consistency/config.py
@@ -59,7 +59,7 @@ class RSEConfiguration(object):
         self.ScannerTimeout = self.ScanerConfig.get("timeout", 300)
         self.RemovePrefix = self.ScanerConfig.get("remove_prefix", "")        # to be applied after site root is removed
         self.AddPrefix = self.ScanerConfig.get("add_prefix", "")              # to be applied after site root is removed
-        self.NWorkers = self.ScanerConfig.get("nworkers", 8))
+        self.NWorkers = self.ScanerConfig.get("nworkers", 8)
         self.IncludeSizes = self.ScanerConfig.get("include_sizes", False)
         self.RecursionThreshold = self.ScanerConfig.get("recursion", 1)
         self.ServerIsRedirector = self.ScanerConfig.get("is_redirector", True)
